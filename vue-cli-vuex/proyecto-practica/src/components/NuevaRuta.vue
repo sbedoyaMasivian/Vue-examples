6886e0752c7492d873e0f04f19cf44dc1f2ec0a4
sbedoyaMasivian <santiago.bedoya@masivian.com>
2019-08-09 16:58:21 -0500

    Ensayo con componentes y vue router

---
+++ vue-cli-vuex/proyecto-practica/src/components/NuevaRuta.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{msj}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NuevaRuta',
+    props: {
+     msj: String
+  }
+}
+</script>
+
---
+++ vue-cli-vuex/proyecto-practica/src/views/Ruta.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="Ruta">
+        <h1>HOLA!</h1>
+        <NuevaRuta msj='Soy componente Ruta'></NuevaRuta>
+    </div>
+</template>
+<script>
+import  NuevaRuta  from "@/components/NuevaRuta.vue"; 
+export default {
+    name: 'Ruta',
+    components:{
+        NuevaRuta
+    }
+}
+</script>
+
+
